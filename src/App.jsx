241311579e5e0eee5baf13f61468c7cb643cf1a1
Saikumar Hangal <saikumarhangal@gmail.com>
2023-01-15 02:30:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import AboutUs from "./Pages/AboutUs";
+import HomePage from "./Pages/HomePage";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className="min-h-screen">
+				<Header />
+				<Routes>
+					<Route path="/" element={<HomePage />} exact />
+					<Route path="/AboutUs" element={<AboutUs />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
+};
+
+export default App;
