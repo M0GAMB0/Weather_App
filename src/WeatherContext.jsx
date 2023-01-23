bd760d13b05cfe029f76bee2435b5a6ac9c0e45b
Saikumar Hangal <saikumarhangal@gmail.com>
2023-01-24 05:03:24 +0530

    weather api fetched

--- src/WeatherContext.jsx
+++ src/WeatherContext.jsx
@@ -1,22 +1,16 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { WEATHER_API_URL } from "./Config/api";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./Config/api";
 
 const Weather = createContext();
 const WeatherContext = ({ children }) => {
 	const [city, setCity] = useState(null);
-	const [lat, setLat] = useState(null);
-	const [long, setLong] = useState(null);
 	const change = (city) => {
 		setCity(city);
-		const [lati, lon] = city.value.split(" ");
-		setLat(lati);
-		setLong(lon);
-		const currentWeatherFetch = `${WEATHER_API_URL}/weather?lat={lat}&lon={lon}&appid={API key}`;
+		const [lat, lon] = city.value.split(" ");
+		const currentWeatherFetch = `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`;
 	};
 	return (
-		<Weather.Provider value={{ city, lat, long, change }}>
-			{children}
-		</Weather.Provider>
+		<Weather.Provider value={{ city, change }}>{children}</Weather.Provider>
 	);
 };
 
