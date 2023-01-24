a16a2da191bd41c5e304611a97275118c3ea27c4
Saikumar Hangal <saikumarhangal@gmail.com>
2023-01-24 06:00:15 +0530

    weather api worked

--- src/WeatherContext.jsx
+++ src/WeatherContext.jsx
@@ -4,11 +4,29 @@ import { WEATHER_API_KEY, WEATHER_API_URL } from "./Config/api";
 const Weather = createContext();
 const WeatherContext = ({ children }) => {
 	const [city, setCity] = useState(null);
+	const [currentWeather, setCurrentWeather] = useState(null);
+	const [foreCast, setForeCast] = useState(null);
+
 	const change = (city) => {
 		setCity(city);
 		const [lat, lon] = city.value.split(" ");
-		const currentWeatherFetch = `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`;
+		const currentWeatherFetch = fetch(
+			`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+		);
+		const foreCastFetch = fetch(
+			`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+		);
+		Promise.all([currentWeatherFetch, foreCastFetch])
+			.then(async (response) => {
+				const weatherResponse = await response[0].json();
+				const forecastResponse = await response[1].json();
+				setCurrentWeather({ city: city.label, ...weatherResponse });
+				setForeCast({ city: city.label, ...forecastResponse });
+			})
+			.catch((err) => console.log(err));
 	};
+	console.log(currentWeather);
+	console.log(foreCast);
 	return (
 		<Weather.Provider value={{ city, change }}>{children}</Weather.Provider>
 	);
